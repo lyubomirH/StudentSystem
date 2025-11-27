29c6c42f479203010c978c7270b14c7a1df3d83a
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2025-11-27 09:19:47 +0200

    new

--- StudentSystem/obj/Debug/net8.0/StudentSystem.AssemblyInfo.cs
+++ StudentSystem/obj/Debug/net8.0/StudentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc70501fefa3f98f5521c7221a68e3a8d75fa40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e703c8b32fe77c3b1642855e09040248a6d2fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
